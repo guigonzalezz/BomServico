dc40068c734cc256d04101adaab1c4e09aacc530
Gustavo Maioli <gumaioli99@gmail.com>
2021-05-08 17:03:04 -0300

    Inserir restante da Entidade

--- bomservico/src/main/java/com/example/demo/bd/entidade/TIpoServico.java
+++ bomservico/src/main/java/com/example/demo/bd/entidade/TIpoServico.java
@@ -2,4 +2,27 @@ package com.example.demo.bd.entidade;
 
 public class TIpoServico {
 
+	private int id;
+	private String nome;
+		
+	public TIpoServico() {
+		super();
+	}
+	public TIpoServico(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
